ce0f54ba4d3ffeefb2a5425849e9c42c7497aae2
tureti123 <132614552+tureti123@users.noreply.github.com>
2024-08-10 21:38:35 +0100

    Delete gare.js

--- gare.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const home= require('./routes/home')
-const app=express()
-app.use(express.json())
-app.use("/home",home)
-const port =process.env.PORT || 3000
-app.listen(port,()=>console.log(port))
